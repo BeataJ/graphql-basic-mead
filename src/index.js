886e655343cbb1129d18f0a0d1c55c2caec6c303
beata <beata@emc2wave.com>
2020-10-14 16:22:17 -0700

    clear index.js delete math.js myModule.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import myCurrentLocation, { message, name, getGreeting } from './myModule';
-import myAdd, { subtract } from './math';
 
-console.log(message);
-console.log(name);
-console.log(myCurrentLocation);
-console.log(getGreeting('bob'));
-console.log(myAdd(4, 5));
-console.log(subtract(5, 4));
