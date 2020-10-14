3af94c95a999ddb97b6d8dbc8c2c72a70ded4446
beata <beata@emc2wave.com>
2020-10-14 16:40:24 -0700

    add Type definations Resolvers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import { GraphQLServer } from 'graphql-yoga';
+
+// Type definations (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+  }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    hello: () => {
+      return 'This is my first query';
+    },
+  },
+};
