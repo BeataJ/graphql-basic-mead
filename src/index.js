16e37129d6fcbfaa4ce2fd7afcb7e75309d65419
beata <beata@emc2wave.com>
2020-10-18 15:48:23 -0700

    delete filds from query

--- src/index.js
+++ src/index.js
@@ -3,29 +3,13 @@ import { GraphQLServer } from 'graphql-yoga';
 // Type definations (schema)
 const typeDefs = `
   type Query {
-    hello: String!
-    name: String!
-    location: String!
-    bio: String!
+    
   }
 `;
 
 // Resolvers
 const resolvers = {
-  Query: {
-    hello: () => {
-      return 'This is my first query';
-    },
-    name: () => {
-      return 'Bob Jasinski';
-    },
-    location: () => {
-      return 'Surray, BC';
-    },
-    bio: () => {
-      return " I'm human";
-    },
-  },
+  Query: {},
 };
 
 const server = new GraphQLServer({
