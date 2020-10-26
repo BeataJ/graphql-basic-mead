f0514628fa30946b0b251facefb8cce13f151f2e
beata <beata@emc2wave.com>
2020-10-25 23:02:34 -0700

    create resolver Post comments

--- src/index.js
+++ src/index.js
@@ -172,6 +172,11 @@ const resolvers = {
         return user.id === parent.author;
       });
     },
+    comments: (parent, args, ctx, info) => {
+      return comments.filter((comment) => {
+        return comment.post === parent.id;
+      });
+    },
   },
   User: {
     posts: (parent, args, ctx, info) => {
