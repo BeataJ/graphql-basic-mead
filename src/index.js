e61b59361a9e5108a02e96d4715863140a206c03
beata <beata@emc2wave.com>
2020-10-27 21:27:20 -0700

    create createPost type Mutation

--- src/index.js
+++ src/index.js
@@ -94,6 +94,7 @@ const typeDefs = `
 
   type Mutation {
     createUser(name:String!, email:String!, age:Int):User!
+    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
   }
 
   type User {
