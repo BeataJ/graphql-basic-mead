b79a95c5ed4c8680d73692f308508191b80099c9
beata <beata@emc2wave.com>
2020-11-04 22:11:19 -0800

    add PubSub

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 import db from './db';
 import Query from './resolvers/Query';
 import Mutation from './resolvers/Mutation';
@@ -6,6 +6,8 @@ import Subscription from './resolvers/Subscription';
 import User from './resolvers/User';
 import Post from './resolvers/Post';
 import Comment from './resolvers/Comment';
+
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
@@ -19,6 +21,7 @@ const server = new GraphQLServer({
   },
   context: {
     db,
+    pubsub,
   },
 });
 
